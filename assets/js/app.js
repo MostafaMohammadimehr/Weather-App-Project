8fede5c0ff89930ba42fe408b79aa9f1bc2369a8
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-09 11:34:25 +0330

    Weather App Project #3

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,11 @@ let cityOutput = document.getElementById("cityoutput");
 let descOutput = document.getElementById("description");
 let tempOutput = document.getElementById("temp");
 let windOutput = document.getElementById("wind");
+const persianRegex = /[\u0600-\u06FF]/;
 const apiKey = "3045dd712ffe6e702e3245525ac7fa38";
+
+cityInput.classList.remove("vijaya");
+cityInput.setAttribute("dir", "rtl");
 
 addInput.addEventListener("click", async () => {
   let weatherResult = await (
@@ -12,13 +16,26 @@ addInput.addEventListener("click", async () => {
       `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric&lang=fa`
     )
   ).json();
-  console.log(weatherResult);
+  setInfo(weatherResult);
 });
+//خلاقیت
+//  خودم از اینجا
 
-//خلاقیت خودم از اینجا
-cityInput.setAttribute("dir", "rtl");
+cityInput.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    let weatherResult = await (
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric&lang=fa`
+      )
+    ).json();
+    setInfo(weatherResult);
+  }
+});
 cityInput.addEventListener("input", () => {
   if (cityInput.value.trim() === "") {
+    cityInput.classList.remove("vijaya");
+    cityInput.setAttribute("dir", "rtl");
+  } else if (persianRegex.test(cityInput.value)) {
     cityInput.classList.remove("vijaya");
     cityInput.setAttribute("dir", "rtl");
   } else {
@@ -26,4 +43,17 @@ cityInput.addEventListener("input", () => {
     cityInput.setAttribute("dir", "ltr");
   }
 });
+
 //تا اینجا
+
+const setInfo = (data) => {
+  let cityName = data["name"];
+  let description = data["weather"][0]["description"];
+  let temp = data["main"]["temp"];
+  let wind = data["wind"]["speed"];
+
+  cityOutput.innerHTML = `City : ${cityName}`;
+  descOutput.innerHTML = `Description : ${description}`;
+  tempOutput.innerHTML = `Temperature : ${temp} °C`;
+  windOutput.innerHTML = `Wind Speed : ${wind} km/h`;
+};
